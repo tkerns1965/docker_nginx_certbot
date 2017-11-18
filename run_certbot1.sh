7f825b743459c7129064e914dec02a13db1aab90
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-18 11:48:04 -0500

    Update run_certbot1.sh

--- run_certbot1.sh
+++ run_certbot1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# docker volume create vol_letsencrypt
+docker volume create letsencrypt
 docker-compose build
 docker-compose up -d
 docker exec -it nginx_certbot1 sh /root/certbot_create.sh
