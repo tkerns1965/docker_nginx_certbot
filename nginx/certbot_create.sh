3d7ce0f08bdd1d74ec886633c142ad95670a8d41
tkerns1965 <tony.kerns.65@gmail.com>
2017-12-14 13:41:29 -0500

    Update certbot_create.sh

--- nginx/certbot_create.sh
+++ nginx/certbot_create.sh
@@ -6,5 +6,8 @@
 # certbot certonly --webroot -n --staging --agree-tos \
 #     -d $CERTBOT_DOMAIN -m $CERTBOT_EMAIL -w $CERTBOT_WEBROOT
 
+# certbot certonly --webroot -n --dry-run --agree-tos \
+#     -d $CERTBOT_DOMAIN -m $CERTBOT_EMAIL -w $CERTBOT_WEBROOT
+
 certbot certonly --webroot -n --agree-tos \
     -d $CERTBOT_DOMAIN -m $CERTBOT_EMAIL -w $CERTBOT_WEBROOT
